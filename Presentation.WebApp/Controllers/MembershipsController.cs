c64224f069dce3c7bd87ee864ea30672586c166d
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-03-16 10:14:29 +0100

    Add project files.

---
+++ Presentation.WebApp/Controllers/MembershipsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers;
+
+public class MembershipsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Presentation.WebApp/Controllers/SupportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers;
+
+public class SupportController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
